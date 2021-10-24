ee496ea378aeb093b79a407fad8f71fa04e8641a
MarcoContreras23 <marcocontreras2309@gmail.com>
2021-10-24 17:32:07 -0500

    Agregar archivos de proyecto.

---
+++ Taller_3/Form1.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Taller_3
+{
+    public partial class Form1 : Form
+    {   
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Variables
+            DateTime ini, fin;
+            System.TimeSpan dur;
+            bool esPrimo = false;
+            long num = 5;
+            //9223372036854775783
+            Thread[] tr = new Thread[3];
+            ini = DateTime.Now;
+            esPrimo = NumberPrime.isPrime(num);
+            fin = DateTime.Now;
+            dur = fin.Subtract(ini);
+
+
+            tr[0] = new Thread(() => NumberPrime.firstDivGreaterThan(num) );
+            tr[1] = new Thread(() => NumberPrime.firstDivMiddle(num));
+            tr[2] = new Thread(() => NumberPrime.isPrime(num));
+            
+            foreach (var thread in tr)
+            {
+                thread.Start();
+                thread.Join();
+            }
+
+            if (esPrimo)
+            {
+                string numero = num.ToString();
+                string rta = numero + " Es primo";
+                label2.Text = rta;
+                string tiempo = dur.ToString();
+                label3.Text = tiempo;
+            }
+            else
+            {
+                string numero = num.ToString();
+                string rta = numero + " No es primo";
+                label2.Text = rta;
+                string tiempo = dur.ToString();
+                label3.Text = tiempo;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Taller_3/calculate/NumberPrime.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Taller_3
+
+/*
+*@author: Marco Contreras
+*/
+
+{
+
+
+    /*
+    *Class in charge of calculating whether the number is prime or not
+    */
+    /// <summary>
+    /// 
+    /// </summary>
+    public class NumberPrime
+    {
+        public long number { get; set; }
+        
+
+        public NumberPrime(long NewNumber)
+        {
+            number = NewNumber;
+        }
+
+       
+
+        public static long firstDivGreaterThan(long num)
+        {
+            int INC = (num % 2 == 1) ? 2 : 1;
+            long lim = (long)Math.Sqrt(num);
+
+            for (long i = 2; i <= lim/2; i += INC)
+            {
+                if (num % i == 0)
+                {
+                    return i;
+                }
+            }
+            return num;
+        }
+
+        public static long firstDivMiddle(long num)
+        {
+            int INC = (num % 2 == 1) ? 2 : 1;
+            long lim = (long)Math.Sqrt(num);
+
+            for (long j = (lim /2)+2; j <= num; j += INC)
+            {
+                if (num % j == 0)
+                {
+                    return j;
+                }
+            }
+            return num;
+        }
+
+
+        public static bool isPrime(long num)
+        {
+
+            return (firstDivGreaterThan(num) == num  && firstDivMiddle(num) == num);
+            //return (firstDivGreaterThan(num) == num);
+        }
+
+
+       
+
+
+
+    }
+}
